e222d727fcd358e29caa3c700a3886610a42cf68
kimdodohyun <qawsed6518@gmail.com>
2018-12-12 20:02:41 +0900

    Update cards.js

--- cards.js
+++ cards.js
@@ -1,22 +1,18 @@
 var counter = 0;
-var cards = [];
 function Card(symbol, value, count) {
     this.count = counter++;
     this.symbol = symbol;
     this.value = value;
 }
 
-
-values = ['a',2,3,4,5,6,7,8,9,10,'k','q','j'];
-symbols = ['c', 'd', 'h', 's'];
-
-for (i = 0; i < values.length; i++) {
-    for (j = 0; i < symbols.length; j++){
-        temp = new Card(symbols[j],values[i]);
-        cards.push(temp);
-    } 
-}
-
-console.log(cards[1].name);
-
+var deck = [
+    new Card('H',1),new Card('H',2),new Card('H',3),new Card('H',4),new Card('H',5),new Card('H',6),new Card('H',7),new Card('H',8),new Card('H',9),new Card('H',10),new Card('H','K'),new Card('H','Q'),new Card('H','J'),
+    new Card('D',1),new Card('D',2),new Card('D',3),new Card('D',4),new Card('D',5),new Card('D',6),new Card('D',7),new Card('D',8),new Card('D',9),new Card('D',10),new Card('D','K'),new Card('D','Q'),new Card('D','J'),
+    new Card('S',1),new Card('S',2),new Card('S',3),new Card('S',4),new Card('S',5),new Card('S',6),new Card('S',7),new Card('S',8),new Card('S',9),new Card('S',10),new Card('S','K'),new Card('S','Q'),new Card('S','J'),
+    new Card('C',1),new Card('C',2),new Card('C',3),new Card('C',4),new Card('C',5),new Card('C',6),new Card('C',7),new Card('C',8),new Card('C',9),new Card('C',10),new Card('C','K'),new Card('C','Q'),new Card('C','J'),
+    new Card('H',1),new Card('H',2),new Card('H',3),new Card('H',4),new Card('H',5),new Card('H',6),new Card('H',7),new Card('H',8),new Card('H',9),new Card('H',10),new Card('H','K'),new Card('H','Q'),new Card('H','J'),
+    new Card('D',1),new Card('D',2),new Card('D',3),new Card('D',4),new Card('D',5),new Card('D',6),new Card('D',7),new Card('D',8),new Card('D',9),new Card('D',10),new Card('D','K'),new Card('D','Q'),new Card('D','J'),
+    new Card('S',1),new Card('S',2),new Card('S',3),new Card('S',4),new Card('S',5),new Card('S',6),new Card('S',7),new Card('S',8),new Card('S',9),new Card('S',10),new Card('S','K'),new Card('S','Q'),new Card('S','J'),
+    new Card('C',1),new Card('C',2),new Card('C',3),new Card('C',4),new Card('C',5),new Card('C',6),new Card('C',7),new Card('C',8),new Card('C',9),new Card('C',10),new Card('C','K'),new Card('C','Q'),new Card('C','J')
+];
 
